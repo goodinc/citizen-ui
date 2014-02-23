d11748587b14c035f585ebd6c494d5075093ab09
Jim Thoburn <jim@jimthoburn.com>
2014-02-22 16:28:05 -0800

    Add example Do buttons, with more than one option

--- assets/shared.js
+++ assets/shared.js
@@ -6,12 +6,34 @@
   ----------------------------------------------- */
   function start() {
 
-    if (!document.querySelector) return;
-
     // OPTIONAL: Show drop-down content on press (instead of hover)
-    new DropDown(document.querySelector("#header .search"));
-    new DropDown(document.querySelector("#header .account"));
-    new DropDown(document.querySelector("#header .todos"));
+    if (document.querySelector) {
+      new DropDown(document.querySelector("#header .search"));
+      new DropDown(document.querySelector("#header .account"));
+      new DropDown(document.querySelector("#header .todos"));
+    }
+
+    // OPTIONAL: Present the do buttons as drop-downs
+    new DoDropDown();
+
+  }
+
+
+  /* Utilities
+  ----------------------------------------------- */
+  function within(needle, haystack) {
+
+    // If the parent element is the target
+    if (needle === haystack) {
+      return true;
+
+    // If any of the children are the target
+    } else if (haystack.firstChild) {
+      var child = haystack.firstChild;
+      do {
+        if (within(needle, child)) return true;
+      } while (child = child.nextSibling);
+    }
 
   }
 
@@ -23,22 +45,6 @@
   (function() {
 
     if (!document.body.addEventListener || !document.querySelector) return;
-
-    function within(needle, haystack) {
-
-      // If the parent element is the target
-      if (needle === haystack) {
-        return true;
-
-      // If any of the children are the target
-      } else if (haystack.firstChild) {
-        var child = haystack.firstChild;
-        do {
-          if (within(needle, child)) return true;
-        } while (child = child.nextSibling);
-      }
-
-    }
 
     DropDown = function(element) {
 
@@ -84,6 +90,82 @@
           hideDetails();
         }
       }, false);
+
+    };
+      
+  })();
+
+
+  /* =DoDropDown
+  ----------------------------------------------- */
+  var DoDropDown = function() {};
+
+  (function() {
+
+    if (!document.body.addEventListener) return;
+
+    function closest(element, nodeName) {
+
+      // If the element is the target
+      if (nodeName === element.nodeName.toLowerCase()) {
+        return element;
+      } else {
+        if (element.parentNode) return closest(element.parentNode, nodeName);
+      }
+
+    }
+
+    DoDropDown = function() {
+
+      if (!document.body.addEventListener) return;
+
+      var html = document.getElementsByTagName("html")[0];
+      html.className += " scripted-do";
+
+      var active;
+      function show(target) {
+        target.className += " active";
+        active = target;
+      }
+
+      function hide(target) {
+        target.className = target.className.replace(/active/g, "");
+        if (active === target) {
+          active = undefined;
+        }
+      }
+
+      function toggle(target) {
+        if (active === target) {
+          hide(active);
+        } else {
+          show(target);
+        }
+      }
+
+      function delegate(e) {
+        var target = e.target;
+
+        if (active) {
+          if (!within(target, active)) {
+            hide(active);
+          }
+        }
+
+        if (target.nodeName.toLowerCase() === "h6") {
+          var form = closest(target, "form");
+          if (form && (form.className.indexOf("do")    >= 0 ||
+                       form.className.indexOf("doing") >= 0 ||
+                       form.className.indexOf("done")  >= 0)) {
+            toggle(form);
+          }
+        }
+      }
+
+      document.body.addEventListener("click", delegate, false);
+
+      // TODO: Handle the case where a user is tabbing between buttons and links on the page
+      //document.body.addEventListener("focus", delegate, false);
 
     };
       
