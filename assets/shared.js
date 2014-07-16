0d7d95a1dfb0a233da8b1b20b6c27368e47ae2eb
Jim Thoburn <jim@jimthoburn.com>
2014-07-16 16:59:57 -0700

    Attempt to get the navigation icon to appear in older Android browsers.

--- assets/shared.js
+++ assets/shared.js
@@ -11,8 +11,10 @@
     new NavDropDown(".post h3", ".post");
     new NavDropDown(".account h3 img", ".account");
 
+
     // OPTIONAL: Present the do, voted, and following buttons as dropdowns.
     new ButtonDropDown();
+
 
     // OPTIONAL: Hide redundant labels in browsers that support the placeholder attribute.
     // http://stackoverflow.com/questions/8263891/simple-way-to-check-if-placeholder-is-supported
@@ -23,6 +25,18 @@
     if (supportsPlaceholder()) {
       var html = document.getElementsByTagName("html")[0];
       html.className += " supports-placeholder";
+    }
+
+
+    // OPTIONAL: Use SVG images if the brower supports them.
+    // http://toddmotto.com/revisiting-svg-workflow-for-performance-and-progressive-development-with-transparent-data-uris
+    // http://css-tricks.com/test-support-svg-img/
+    function supportsSvg() {
+      return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
+    }
+    if (supportsSvg()) {
+      var html = document.getElementsByTagName("html")[0];
+      html.className += " supports-svg";
     }
 
 
